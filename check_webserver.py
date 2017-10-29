f0cb75342b6a0f55e3b605402b9af58fedf232a4
KevFan <kevin_fan@hotmail.co.uk>
2017-10-29 22:03:58 +0000

    Add check if error is from nginx not installed + add install nginx function

--- check_webserver.py
+++ check_webserver.py
@@ -4,11 +4,13 @@ import subprocess
 
 # Function to start nginx (assuming it has been installed)
 def start_nginx():
-    startCmd = 'sudo service nginx start'
-    print('Going to start nginx with: ' + startCmd)
-    (status, output) = subprocess.getstatusoutput(startCmd)
+    start_cmd = 'sudo service nginx start'
+    print('Going to start nginx with: ' + start_cmd)
+    (status, output) = subprocess.getstatusoutput(start_cmd)
     if status == 0:
         print ('Nginx is now up and running')
+    elif 'nginx: unrecognized service' in output:
+        install_nginx()
     else:
         print ('Start nginx - something is wrong :(')
         print (status, output)
@@ -25,10 +27,26 @@ def check_nginx():
         start_nginx()
 
 
+# Function to install nginx if not already installed
+def install_nginx():
+    print ('Nginx not installed')
+    choice = input('Install nginx (y/n): ').lower()
+    if choice == 'y':
+        install_cmd = 'sudo yum install -y nginx'
+        (status, output) = subprocess.getstatusoutput(install_cmd)
+        if status == 0:
+            print ('Nginx installed')
+            start_nginx()
+        else:
+            print ('Nginx install error')
+            print (output)
+
+
 # Main funcion
 def main():
     check_nginx()
 
 
+# This is the standard boilerplate that calls the main() function.
 if __name__ == "__main__":
     main()
